f3fe28be5d437e5089f3e735d804f16bc5a78501
alamtopan <alamtopan@alamtopan-HP-Pavilion-14-Notebook-PC.(none)>
2013-12-05 10:24:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigTv::Application.config.secret_token = '62d3256b9900de60d42a51c97125263b347960465a67062217b458f567a494ab9d415ded354923468c4a2d025464263a8ccd76bb62177c75a64b5d0e3a0fada3'
