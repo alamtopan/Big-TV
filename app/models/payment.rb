d9f3b08308cc3c99e8f96a714c056d932fc83b94
Wawan Kurniawan <ones07@gmail.com>
2014-01-13 16:30:14 +0700

    payment: RESPONSECODE == '0000' ? SUCCESS : FAILED

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,23 +2,25 @@ class Payment < ActiveRecord::Base
   belongs_to :order
 
   def self.track_payment(order, options, req_env)
-     payment = Payment.new
-     payment.order_id       = order.id
-     payment.transaction_no = options[:PAYMENTCODE]
-     payment.status         = options[:VERIFYSTATUS].to_s.upcase
-     payment.bank_issuer    = options[:BANK]
-     payment.credit_card    = options[:MCN]
-     payment.track_record   = "#{options}"
-     payment.access_record  = "#{req_env}"
-     payment.total          = options[:AMOUNT].to_f
+    payment_status = options[:RESPONSECODE] === '0000' ? 'SUCCESS' : 'FAILED'
 
-     order.status = options[:VERIFYSTATUS].to_s.upcase
-     if payment.save && order.save
-      CustomerMailer.delay.payment_email(order, payment)
-      CustomerMailer.delay.email_order_to_admin(order)
-      return true
+    payment = Payment.new
+    payment.order_id       = order.id
+    payment.transaction_no = options[:PAYMENTCODE]
+    payment.status         = payment_status
+    payment.bank_issuer    = options[:BANK]
+    payment.credit_card    = options[:MCN]
+    payment.track_record   = "#{options}"
+    payment.access_record  = "#{req_env}"
+    payment.total          = options[:AMOUNT].to_f
+
+    order.status = payment_status
+    if payment.save && order.save
+    CustomerMailer.delay.payment_email(order, payment)
+    CustomerMailer.delay.email_order_to_admin(order)
+    return true
     else
-      return false
+    return false
     end
   end
 
