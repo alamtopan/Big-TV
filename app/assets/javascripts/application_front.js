1e16f7e190767fac60be37f2e49346eea9bfb09a
Yacobus Reinhart <yacobus.reinhart@gmail.com>
2014-02-10 21:26:34 +0700

    revised slider, add google analytic, revise blog

--- app/assets/javascripts/application_front.js
+++ app/assets/javascripts/application_front.js
@@ -272,4 +272,16 @@ $(document).ready(function(){
       return "Others";
     }
   }
+   
+  var hostName = window.location.host;
+  if(hostName == "big-tv.com" || hostName == "www.big-tv.com"){
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-47830911-1', 'big-tv.com');
+    ga('send', 'pageview');
+  }
+  
 });
