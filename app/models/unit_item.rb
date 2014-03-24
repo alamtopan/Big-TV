b4f7bac1efe3effc97fd3c3db5e44a8fac0b3319
alamtopan <alamtopan@alamtopan-HP-Pavilion-14-Notebook-PC.(none)>
2014-03-24 10:03:10 -0400

    pull

--- app/models/unit_item.rb
+++ app/models/unit_item.rb
@@ -1,13 +1,13 @@
 class UnitItem < ActiveRecord::Base
   # Attributess dari model unit items  
-  attr_accessible :group_item_id, :name, :logo,:status_hd, :free
+  attr_accessible :group_item_id, :name, :logo,:status_hd, :free, :position
   has_attached_file :logo, styles:  { 
                                       :medium => "300x300>", 
                                       :thumb => "100x100>" 
                                     }, 
                                     :default_url => "/assets/no-image.png" # Role field logo
 
-  acts_as_list
+  acts_as_list scope: :group_item
 
   # Scope query
   scope :by_position, order('position ASC')
