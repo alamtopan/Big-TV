67331fbc1792e281628f625d0fe8cfc0465eff94
alamtopan <alamtopani@yahoo.com>
2014-04-24 05:26:43 -0400

    revision job apllicant

--- app/models/job_applicant.rb
+++ app/models/job_applicant.rb
@@ -4,7 +4,6 @@ class JobApplicant < ActiveRecord::Base
 
   belongs_to :job, foreign_key: "position"
 
-  validates :name, :position, :email
   validates_attachment :file_resume, size: {less_than: 2.megabytes}
   validates_format_of :email, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
 
